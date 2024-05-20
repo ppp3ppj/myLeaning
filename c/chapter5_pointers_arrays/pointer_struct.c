07ba62ce9817aeb6b3ad277eae4232a35baa9eb0
ppp3ppj <ppp.3ppj@gmail.com>
2024-05-20 23:19:16 +0700

    selft-referentail structures

---
+++ c/chapter5_pointers_arrays/pointer_struct.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#define MAXWORD 100
+
+struct tnode {
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnode *right;
+};
+
+// Prototype
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+int getword(char *, int);
+
+int main() {
+    struct tnode *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while(getword(word, MAXWORD) != EOF)
+        if(isalpha(word[0]))
+            root = addtree(root, word);
+    treeprint(root);
+    return 0;
+}
+
