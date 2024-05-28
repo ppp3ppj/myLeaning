249c2856385bad2a12aeebe7d97e232a5305c0c2
ppp3ppj <ppp.3ppj@gmail.com>
2024-05-28 22:18:09 +0700

    add customer and test customer basic

--- go/impl-ddd/aggregate/customer.go
+++ go/impl-ddd/aggregate/customer.go
@@ -1,1 +1,41 @@
 package aggregate
+
+import (
+	"errors"
+	"impl-ddd/entity"
+	"impl-ddd/valueobject"
+
+	"github.com/google/uuid"
+)
+
+var (
+    ErrInvalidPerson = errors.New("a customer has to have a valid name")
+)
+
+type Customer struct {
+    // peson is the root entity of customer
+    // which mean person.Id is the main identifier for the customer
+
+    person *entity.Person
+    products []*entity.Item
+    transactions []valueobject.Transaction // not ptr cause it imutable - valueobject
+}
+
+// NewCustomer is a factory to create a new customer aggregate
+// validate that the name is not empty
+func NewCustomer(name string) (Customer, error) {
+    if name == "" {
+        return Customer{}, ErrInvalidPerson
+    }
+
+    person := &entity.Person{
+        Name: name,
+        Id: uuid.New(),
+    }
+
+    return Customer{
+        person: person,
+        products: make([]*entity.Item, 0),
+        transactions: make([]valueobject.Transaction, 0),
+    }, nil
+}
