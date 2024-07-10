9bd40b0c5678db808178ebe97d085bfa4b2fecc6
ppp3ppj <ppp.3ppj@gmail.com>
2024-07-10 22:19:31 +0700

    add vector in zig

---
+++ zig/zig101/variables/arrays/test_vector.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+test "Basic vector usage" {
+    const a = @Vector(4, i32){ 1, 2, 3, 4 };
+    const b = @Vector(4, i32){ 5, 6, 7, 8 };
+
+    const c = a + b;
+
+    try expectEqual(6, c[0]);
+    try expectEqual(8, c[1]);
+    try expectEqual(10, c[2]);
+    try expectEqual(12, c[3]);
+}
+
+test "Conversion between vectors, arrays, and slices" {
+    const arr1: [4]f32 = [_]f32{ 1.1, 3.2, 4.5, 5.6 };
+    const vec: @Vector(4, f32) = arr1;
+    const arr2: [4]f32 = vec;
+    try expectEqual(arr1, arr2);
+
+    const vec2: @Vector(2, f32) = arr1[1..3].*;
+
+    const slice: []const f32 = &arr1;
+    var offset: u32 = 1;
+    _ = &offset;
+
+    const vec3: @Vector(2, f32) = slice[offset..][0..2].*;
+    try expectEqual(slice[offset], vec2[0]);
+    try expectEqual(slice[offset + 1], vec2[1]);
+    try expectEqual(vec2, vec3);
+}
