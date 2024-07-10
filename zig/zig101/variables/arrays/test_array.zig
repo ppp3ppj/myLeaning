011e160524e18aa8d08e6e78bc86cb4d2a6eda56
ppp3ppj <ppp.3ppj@gmail.com>
2024-07-10 21:58:18 +0700

    add array initialization in comptime

--- zig/zig101/variables/arrays/test_array.zig
+++ zig/zig101/variables/arrays/test_array.zig
@@ -71,3 +71,38 @@ comptime {
 }
 
 // use compile-time to initialize an array
+var fancy_array = init: {
+    var initial_value : [10]Point = undefined;
+    for (&initial_value, 0..) |*pt, i| {
+        pt.* = Point{
+            .x = @intCast(i),
+            .y = @intCast(i * 2),
+        };
+    }
+    break :init initial_value;
+};
+
+const Point = struct {
+    x: i32,
+    y: i32,
+};
+
+test "compile-time array initialization" {
+    try expect(fancy_array[4].x == 4);
+    try expect(fancy_array[4].y == 8);
+}
+
+// call a function to initialize an array
+var more_points = [_]Point{makePoint(3)} ** 10;
+fn makePoint(x: i32) Point {
+    return Point{
+        .x = x,
+        .y = x * 2,
+    };
+}
+
+test "array initialization with function calls" {
+    try expect(more_points[4].x == 3);
+    try expect(more_points[4].y == 6);
+    try expect(more_points.len == 10);
+}
