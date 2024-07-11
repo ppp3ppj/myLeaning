a126b6a5d0857ff3d6aaee5330d3b34f97a4ae6c
ppp3ppj <ppp.3ppj@gmail.com>
2024-07-11 22:56:15 +0700

    add example single and many item pointer basic

---
+++ zig/zig101/variables/pointers/test_pointer_arithmetic.zig
@@ -0,0 +1,27 @@
+const expect = @import("std").testing.expect;
+
+test "pointer arithmetic with many-item pointer" {
+    const array = [_]i32{ 1, 2, 3, 4 };
+    var ptr: [*]const i32 = &array;
+
+    try expect(ptr[0] == 1);
+    ptr += 1;
+    try expect(ptr[0] == 2);
+
+    try expect(ptr[1..] == ptr + 1);
+}
+
+test "pointer arithmetic with slices" {
+    var array = [_]i32{ 1, 2, 3, 4 };
+    var length: usize = 0;
+    _ = &length;
+    var slice = array[length..array.len];
+
+    try expect(slice[0] == 1);
+    try expect(slice.len == 4);
+
+    slice.ptr += 1;
+
+    try expect(slice[0] == 2);
+    try expect(slice.len == 4);
+}
