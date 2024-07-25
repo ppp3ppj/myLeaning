6b1954c704eddadff6f0fba76d94370e86dce688
ppp3ppj <ppp.3ppj@gmail.com>
2024-07-25 21:40:27 +0700

    add module attributes example in elixir

---
+++ elixir/elixir101/module_attributes/math.ex
@@ -0,0 +1,16 @@
+defmodule Math do
+  @moduledoc """
+  Provides math-related functions.
+
+  ## Example
+
+    iex> Math.sum(1, 2)
+    3
+
+  """
+
+  @doc """
+  Calculate the sum of two numbers
+  """
+  def sum(a, b), do: a + b
+end
