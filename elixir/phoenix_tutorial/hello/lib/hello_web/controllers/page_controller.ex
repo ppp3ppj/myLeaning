ee52b393d7ee90d966e777eefdd09598f2f680fa
ppp3ppj <ppp.3ppj@gmail.com>
2024-08-06 21:46:11 +0700

    add static index page example basic

--- elixir/phoenix_tutorial/hello/lib/hello_web/controllers/page_controller.ex
+++ elixir/phoenix_tutorial/hello/lib/hello_web/controllers/page_controller.ex
@@ -6,4 +6,8 @@ defmodule HelloWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def index(conn, _params) do
+    render(conn, :index)
+  end
 end
